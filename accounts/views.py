825622a595bbdf4898e31495b7a3cac48876f932
EctroDEV <ectroshop9@gmail.com>
2025-12-09 02:12:22 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,41 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer, MyTokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.http import JsonResponse
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = MyTokenObtainPairSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 def health(request):
     return JsonResponse({
         "project": "serialcoTV",
-        "status": "API is running successfully running",
+        "status": "API is running",
         "database": "PostgreSQL ready"
     })
