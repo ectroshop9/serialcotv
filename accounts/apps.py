164f09a67b71fe667e95a7bd1b6f248d41a11364
EctroDEV <ectroshop9@gmail.com>
2025-12-09 04:22:03 +0100

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,11 @@
+# accounts/apps.py
 from django.apps import AppConfig
-
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+    verbose_name = 'حسابات العملاء'
+    
+    def ready(self):
+        # استيراد الإشارات عند تشغيل التطبيق
+        import accounts.signals
