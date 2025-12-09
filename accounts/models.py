e879bd46becb724e75bd7fea3345478df70faf51
EctroDEV <ectroshop9@gmail.com>
2025-12-09 02:09:50 +0100

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # إذا عايز تضيف حقول إضافية (مثل صورة أو رقم تليفون)
+    pass
