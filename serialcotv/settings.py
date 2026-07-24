eda619920dd87f4779f3c774a2b3d62c38d1c169
ectrodev <ectroshop9@gmail.com>
2026-07-24 20:46:35 +0200

    Update settings.py

--- serialcotv/settings.py
+++ serialcotv/settings.py
@@ -185,6 +185,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 5
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = 'SerialCo TV <noreply@serialco.tv>'
