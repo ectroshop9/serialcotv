a07138d6bcaf6c9201ca9e4eb1364bab9efdcd1a
ectrodev <ectroshop9@gmail.com>
2026-07-24 16:44:23 +0200

    Update serializers.py

--- serials/serializers.py
+++ serials/serializers.py
@@ -2,46 +2,9 @@ from rest_framework import serializers
 from .models import SerialKey, SerialPackage, SerialUsage
 
 
-class CheckSerialSerializer(serializers.Serializer):
-    serial = serializers.CharField(max_length=32, required=False)
-    serial_number = serializers.CharField(max_length=32, required=False)
-    pin = serializers.CharField(max_length=8, required=True)
-
-    def validate(self, data):
-        if not data.get('serial') and not data.get('serial_number'):
-            raise serializers.ValidationError("يرجى إدخال السيريال")
-        return data
-
-
 class SerialVerifySerializer(serializers.Serializer):
     serial_number = serializers.CharField(max_length=32, required=True)
     pin = serializers.CharField(max_length=8, required=True)
-
-
-class ActivateSerialSerializer(serializers.Serializer):
-    serial = serializers.CharField(max_length=32, required=False)
-    serial_number = serializers.CharField(max_length=32, required=False)
-    pin = serializers.CharField(max_length=8, required=True)
-    customer_id = serializers.IntegerField(required=True)
-
-    def validate(self, data):
-        if not data.get('serial') and not data.get('serial_number'):
-            raise serializers.ValidationError("يرجى إدخال السيريال")
-        return data
-
-
-class UseTokenSerializer(serializers.Serializer):
-    serial = serializers.CharField(max_length=32, required=False)
-    serial_number = serializers.CharField(max_length=32, required=False)
-    pin = serializers.CharField(max_length=8, required=True)
-    file_name = serializers.CharField(max_length=255, required=False, allow_blank=True)
-    file_type = serializers.CharField(max_length=50, required=False, default='firmware')
-    token_amount = serializers.IntegerField(required=False, default=1)
-
-    def validate(self, data):
-        if not data.get('serial') and not data.get('serial_number'):
-            raise serializers.ValidationError("يرجى إدخال السيريال")
-        return data
 
 
 class SerialDownloadSerializer(serializers.Serializer):
